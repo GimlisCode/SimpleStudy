4106decf00686959dfbe5923e572491072827470
Johannes Maul <johannes.maul@gmx.de>
2020-11-06 15:08:41 +0100

    UML Update + Verwaltung als Überklasse

--- Code/SimpleStudy.Schicht3.Application/src/Modifier/HochschuleModifier.java
+++
@@ -1,5 +0,0 @@
-package Modifier;
-
-public class HochschuleModifier {
-
-}
---
+++ Code/SimpleStudy.Schicht3.Application/src/Modifier/Verwaltung.java
@@ -0,0 +1,24 @@
+package Modifier;
+
+import Models.Entity;
+import java.util.HashMap;
+
+
+abstract class Verwaltung {
+	
+	HashMap<Integer,Entity> entities;
+	
+	public Verwaltung() {
+		entities = new HashMap<>();
+	}
+	
+	public void add(Entity entity) {
+		entities.put(entity.getId(), entity);
+	}
+	
+	public void update(Entity entity) {
+	}
+	
+	a
+	
+}

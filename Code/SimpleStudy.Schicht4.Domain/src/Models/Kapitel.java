07a85d6542c42e0d1fd401de31832ccbde2dd7af
Johannes Maul <johannes.maul@gmx.de>
2021-01-12 17:08:00 +0100

    Domain-Klassen erweitert mit getterSetterAddRemove

--- Code/SimpleStudy.Schicht4.Domain/src/Models/Kapitel.java
+++ Code/SimpleStudy.Schicht4.Domain/src/Models/Kapitel.java
@@ -6,16 +6,54 @@ public class Kapitel extends Entity{
 	
 	private String name;
 	private int nr;
-	private ArrayList<Frage> Fragen = new ArrayList<>();
+	private ArrayList<Frage> fragen = new ArrayList<>();
 	
 	
 	public Kapitel(String name, int nr, ArrayList<Frage> fragen) {
 		super();
 		this.name = name;
 		this.nr = nr;
-		Fragen = fragen;
+		this.fragen = fragen;
 	}
 
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getNr() {
+		return nr;
+	}
+
+
+	public void setNr(int nr) {
+		this.nr = nr;
+	}
+
+
+	public ArrayList<Frage> getFragen() {
+		return fragen;
+	}
+
+
+	public void setFragen(ArrayList<Frage> fragen) {
+		this.fragen = fragen;
+	}
+
+	public void add(Frage newFrage) {
+		if (!fragen.contains(newFrage))		
+			fragen.add(newFrage);		
+	}
 	
+	public void remove(Frage removeFrage) {
+		if (fragen.contains(removeFrage))
+			fragen.remove(removeFrage);
+	}
 	
 }

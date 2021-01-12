3bbec60503fe109f6d6aec10221ceb9b016b8097
Johannes Maul <johannes.maul@gmx.de>
2021-01-12 16:37:07 +0100

    Array Remove and Add mfor Statistik in Student

--- Code/SimpleStudy.Schicht4.Domain/src/Models/Student.java
+++ Code/SimpleStudy.Schicht4.Domain/src/Models/Student.java
@@ -7,4 +7,30 @@ public class Student extends Entity{
 	private String name;
 	private ArrayList<Statistik> statistik = new ArrayList<>();
 	
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Statistik> getStatistik() {
+		return statistik;
+	}
+	public void setStatistik(ArrayList<Statistik> statistik) {
+		this.statistik = statistik;
+	}
+	
+	public void add(Statistik newStatistik) {
+		if (statistik.indexOf(newStatistik) != -1)		
+			statistik.add(newStatistik);		
+	}
+	
+	public void remove(Statistik removeStatistik) {
+		int index = statistik.indexOf(removeStatistik);
+		if (index != -1)
+			statistik.remove(index);
+	}
 }

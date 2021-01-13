987563029b517e976a93cae9627e09e33d9b199d
Johannes Maul <johannes.maul@gmx.de>
2021-01-13 09:22:24 +0100

    CRUD in StatistikModel

--- Code/SimpleStudy.Schicht4.Domain/src/Models/Statistik.java
+++ Code/SimpleStudy.Schicht4.Domain/src/Models/Statistik.java
@@ -10,4 +10,26 @@ public class Statistik {
 	
 	//DOKU: Tupel ersetzt durch Wrapper
 	private Map<Integer, Richtigkeit> statistik = new HashMap<>();
+
+	public Map<Integer, Richtigkeit> getStatistik() {
+		return statistik;
+	}
+
+	public void setStatistik(Map<Integer, Richtigkeit> statistik) {
+		this.statistik = statistik;
+	}
+	
+
+	public void add(Integer frageId, Richtigkeit richtigkeit)
+	{		
+		statistik.put(frageId, richtigkeit);
+	}
+	
+	public void remove(Integer frageId, Boolean richtigkeit)
+	{		
+		if (statistik.containsKey(frageId)) 
+			statistik.remove(frageId);
+	}
+
+
 }

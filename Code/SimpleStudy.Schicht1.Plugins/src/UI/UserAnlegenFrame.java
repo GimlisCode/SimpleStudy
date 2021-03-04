bfe3d67d8c1fe09ea5f708c096fee855b0c6ae6d
SimonLickteig <66003970+SimonLickteig@users.noreply.github.com>
2021-03-04 09:22:02 +0100

    UserAnlegenFrame Student wird angelegt wenn Eintrag korrekt

--- Code/SimpleStudy.Schicht1.Plugins/src/UI/UserAnlegenFrame.java
+++ Code/SimpleStudy.Schicht1.Plugins/src/UI/UserAnlegenFrame.java
@@ -4,12 +4,17 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import Controller.MainController;
+import Models.Student;
 
 public class UserAnlegenFrame extends JFrame implements ActionListener
 
@@ -23,12 +28,13 @@ public class UserAnlegenFrame extends JFrame implements ActionListener
 
 	public UserAnlegenFrame()
 	{
+		this.setTitle("Neuer Student*in");
 
 		this.setLayout(new BorderLayout());
 
 		pn_eingaben.setLayout(new GridLayout(1, 1));
 		tf_benutzer = new JTextField();
-		lbl_benutzer = new JLabel("Name des Studenten:");
+		lbl_benutzer = new JLabel("Name des Studierenden:");
 		pn_eingaben.add(lbl_benutzer);
 		pn_eingaben.add(tf_benutzer);
 		this.add(pn_eingaben,
@@ -41,7 +47,8 @@ public class UserAnlegenFrame extends JFrame implements ActionListener
 				BorderLayout.SOUTH);
 
 		this.setVisible(true);
-		this.pack();
+		this.setSize(300,
+				150);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	}
@@ -49,7 +56,25 @@ public class UserAnlegenFrame extends JFrame implements ActionListener
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		final var benutzer = tf_benutzer.getText();
+		if (tf_benutzer.getText()
+				.isEmpty())
+		{
+			JOptionPane fehlermeldung = new JOptionPane();
+			fehlermeldung.showMessageDialog(null,
+					"Sie müssen einen Namen eingeben",
+					"Fehler",
+					JOptionPane.WARNING_MESSAGE);
+		}
+
+		else
+		{
+			final var benutzer = tf_benutzer.getText();
+
+			HashMap<String, String> student = new HashMap<>();
+			student.put(Student.nameText,
+					benutzer);
+			MainController.createStudent(student);
+		}
 
 	}
 

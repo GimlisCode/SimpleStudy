8b34cada09678a289e4df6745c8d9ce3b55c03a5
SimonLickteig <66003970+SimonLickteig@users.noreply.github.com>
2021-03-04 10:56:39 +0100

    getHochchuleForView implementiert

--- Code/SimpleStudy.Schicht2.Adapters/src/Renderer/HochschulRenderer.java
+++ Code/SimpleStudy.Schicht2.Adapters/src/Renderer/HochschulRenderer.java
@@ -1,5 +1,29 @@
 package Renderer;
 
-public class HochschulRenderer {
+import java.util.ArrayList;
+import java.util.Collection;
+
+import Models.Hochschule;
+
+public class HochschulRenderer
+{
+
+	public static ArrayList<PrettyHashMap> getHochschuleForView(Collection<Hochschule> hochschulen)
+	{
+
+		ArrayList<PrettyHashMap> gerenderteHochschulen = new ArrayList<>();
+		for (Hochschule hochschule : hochschulen)
+		{
+			PrettyHashMap gerenderteHochschule = new PrettyHashMap();
+			gerenderteHochschule.put(Hochschule.idText,
+					hochschule.getId() + "");
+			gerenderteHochschule.put(Hochschule.nameText,
+					hochschule.getName());
+
+			gerenderteHochschulen.add(gerenderteHochschule);
+		}
+
+		return gerenderteHochschulen;
+	}
 
 }

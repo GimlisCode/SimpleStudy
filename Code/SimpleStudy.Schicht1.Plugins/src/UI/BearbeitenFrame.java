230924a20bc224e2eb2d3350e9e52f945c0202c0
SimonLickteig <66003970+SimonLickteig@users.noreply.github.com>
2021-03-11 12:00:35 +0100

    Create BearbeitenFrame.java

---
+++ Code/SimpleStudy.Schicht1.Plugins/src/UI/BearbeitenFrame.java
@@ -0,0 +1,23 @@
+package UI;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import Models.Hochschule;
+
+public class BearbeitenFrame extends JFrame
+{
+	JLabel lbl_id;
+
+	public BearbeitenFrame(Object model)
+	{
+		this.setTitle("Bearbeiten");
+
+		if (model == Hochschule.class.getSimpleName())
+		{
+
+		}
+
+	}
+
+}
